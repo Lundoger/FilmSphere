a811fde28b8146324d7420f69461586483806e4d
Богдан <sysoevbogdan82@gmail.com>
2024-01-23 20:06:23 +0200

    created a search-modal list

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	images: {
+		unoptimized: true,
+		domains: ['st.kp.yandex.net', 'avatars.mds.yandex.net', 'themoviedb.org'],
+	},
+	env: {
+		NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+	},
+	reactStrictMode: true,
 }
 
 export default nextConfig
--- src/api/filmSphereApi.ts
+++ src/api/filmSphereApi.ts
@@ -1,28 +1,28 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import {} from "src/models/IMovie";
+import {SearchMovies } from "@/models/Api";
 
 export const filmSphereApi = createApi({
-  reducerPath: "filmSphereApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "https://api.kinopoisk.dev/",
-  }),
-  endpoints: (build) => ({
-    getByName: build.query<any, any>({
-      query: (query: string, page: number = 1, limit: number = 20) => ({
-        url: "v1.4/movie/search",
-        method: "GET",
-        headers: {
-          accept: "application/json",
-          "X-API-KEY": process.env.API_KEY,
-        },
-        params: {
-          page: page,
-          limit: limit,
-          query: query,
-        },
-      }),
-    }),
-  }),
-});
+	reducerPath: "filmSphereApi",
+	baseQuery: fetchBaseQuery({
+		baseUrl: "https://api.kinopoisk.dev/",
+	}),
+	endpoints: (build) => ({
+		getSearchTitle: build.query<SearchMovies, string>({
+			query: (query: string, page: number = 1, limit: number = 20) => ({
+				url: "v1.2/movie/search",
+				method: "GET",
+				headers: {
+					accept: "application/json",
+					'X-API-KEY': process.env.NEXT_PUBLIC_API_KEY,
+				},
+				params: {
+					page: page,
+					limit: limit,
+					query: query,
+				},
+			}),
+		}),
+	}),
+})
 
-export const { useGetByNameQuery, useLazyGetByNameQuery } = filmSphereApi;
+export const { useGetSearchTitleQuery, useLazyGetSearchTitleQuery } = filmSphereApi
