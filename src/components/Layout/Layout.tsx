57a0b4c184c93f59e136a0d0745654a8c98c9302
Богдан <sysoevbogdan82@gmail.com>
2024-02-02 15:14:59 +0200

    added HeroSlider folder

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,12 +2,12 @@ import { FC, PropsWithChildren } from "react";
 import Nav from "@/components/Nav/Nav";
 import SearchModal from "../SearchModal/SearchModal";
 
-export const Layout: FC<PropsWithChildren<{}>> = ({ children }) => {
-  return (
-    <>
-      <Nav />
-      <main className="main">{children}</main>
-	  <SearchModal/>
-    </>
-  );
-};
+export const Layout: FC<PropsWithChildren> = ({ children }) => {
+	return (
+		<>
+			<Nav />
+			<main className="main">{children}</main>
+			<SearchModal />
+		</>
+	)
+}
