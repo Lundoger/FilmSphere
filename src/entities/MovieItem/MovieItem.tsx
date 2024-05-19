69e0994b65c694e152371e2e87aec99729335955
Богдан <sysoevbogdan82@gmail.com>
2024-05-19 16:28:56 +0300

    added pagination for catalog

--- src/entities/MovieItem/MovieItem.tsx
+++ src/entities/MovieItem/MovieItem.tsx
@@ -9,49 +9,44 @@ import Image from "next/image"
 import Link from "next/link"
 
 interface MovieItemProps {
-    item: MovieDtoV14 | LinkedMovieV14 | MovieInPerson
-    rating?: number | null
-    className?: string
+  item: MovieDtoV14 | LinkedMovieV14 | MovieInPerson
+  rating?: number | null
+  className?: string
 }
 
 const MovieItem = ({ item, className, rating }: MovieItemProps) => {
-    const movieRating = Number(rating ?? getRating(item?.rating as Rating)).toFixed(1)
-    const name = getTitleName(item.name)
+  const movieRating = Number(rating ?? getRating(item?.rating as Rating)).toFixed(1)
+  const name = getTitleName(item.name)
 
-    let imageSrc = `https://image.openmoviedb.com/kinopoisk-st-images//actor_iphone/iphone360_4097096.jpg`
-    if ("poster" in item && item.poster?.url) imageSrc = item.poster?.url
+  let imageSrc = `https://image.openmoviedb.com/kinopoisk-st-images//actor_iphone/iphone360_4097096.jpg`
+  if ("poster" in item && item.poster?.url) imageSrc = item.poster?.url
 
-    return (
-        <Link className={clsx("title-item", className)} href={paths.movie(item.id)}>
-            <div className="title-item__bg">
-                <Image
-                    priority
-                    alt={item?.name ?? "Нет названия (ಥ_ಥ)"}
-                    className="title-item__bg-img"
-                    fill
-                    quality={100}
-                    sizes="100%"
-                    src={imageSrc}
-                />
-            </div>
-            <div className="title-item__content">
-                <MovieRating className="title-item__rating">{movieRating}</MovieRating>
-                <div className="title-item__info">
-                    <h3 className="title-item__name">{name}</h3>
-                    <div className="title-item__row">
-                        {"year" in item && item?.year && (
-                            <span className="title-item__label">{item.year}</span>
-                        )}
-                        {"movieLength" in item && item?.movieLength && (
-                            <span className="title-item__label">
-                                {minutesToHour(item.movieLength)}
-                            </span>
-                        )}
-                    </div>
-                </div>
-            </div>
-        </Link>
-    )
+  return (
+    <Link className={clsx("title-item", className)} href={paths.movie(item.id)}>
+      <div className="title-item__bg">
+        <Image
+          alt={item?.name ?? "Нет названия (ಥ_ಥ)"}
+          className="title-item__bg-img"
+          fill
+          quality={100}
+          sizes="100%"
+          src={imageSrc}
+        />
+      </div>
+      <div className="title-item__content">
+        <MovieRating className="title-item__rating">{movieRating}</MovieRating>
+        <div className="title-item__info">
+          <h3 className="title-item__name">{name}</h3>
+          <div className="title-item__row">
+            {"year" in item && item?.year && <span className="title-item__label">{item.year}</span>}
+            {"movieLength" in item && item?.movieLength && (
+              <span className="title-item__label">{minutesToHour(item.movieLength)}</span>
+            )}
+          </div>
+        </div>
+      </div>
+    </Link>
+  )
 }
 
 export default MovieItem
--- src/widgets/Catalog/Catalog.tsx
+++ src/widgets/Catalog/Catalog.tsx
@@ -1,50 +1,68 @@
-import { useGetGenreTitleQuery } from "@/api/filmSphereApi"
+import { useLazyGetGenreTitleQuery } from "@/api/filmSphereApi"
 import MovieItem from "@/entities/MovieItem/MovieItem"
 import Spinner from "@/shared/Spinner/Spinner"
 import clsx from "clsx"
 import { useRouter } from "next/router"
 import { paramsToObject } from "./lib/paramsToObject"
 import { Button } from "@/shared/Button/Button"
+import { Pagination } from "./ui/Pagination/Pagination"
+import { usePageChange } from "./ui/Pagination/lib/usePageChange"
+import { useEffect } from "react"
 
 interface CatalogProps {
-    title: string
-    className?: string
-    type?: string
+  title: string
+  className?: string
+  type?: string
 }
 
 const Catalog = ({ title, className, type }: CatalogProps) => {
-    const { query } = useRouter()
-    const params = paramsToObject(
-        query.genre as string,
-        query.year as string,
-        query.sortField as string
-    )
-    const { data, isLoading, isError } = useGetGenreTitleQuery({ ...params, type, limit: 40 })
+  const { query, push } = useRouter()
+  const params = paramsToObject(
+    query.genre as string,
+    query.year as string,
+    query.sortField as string
+  )
+  const page = Number(query.page ?? 1)
+  const [fetchGenreTitle, { data, isFetching, isError }] = useLazyGetGenreTitleQuery()
 
-    return (
-        <section className={clsx("catalog", className)}>
-            <div className="catalog__container">
-                <h1 className="catalog__title">{title}</h1>
-                <div className="catalog__filters">
-                    <Button>Фильтры</Button>
-                    <span>сортировать по: (select)</span>
-                </div>
-                {isLoading && !data && (
-                    <div className="loader--container">
-                        <Spinner />
-                    </div>
-                )}
-                {isError && <p className="title--error">Упс.. Что-то пошло не так (⌣̩̩́_⌣̩̩̀)</p>}
-                {data && (
-                    <div className="catalog__movies-list">
-                        {data?.docs.map((item, i) => (
-                            <MovieItem className="catalog__movies-item" key={i} item={item} />
-                        ))}
-                    </div>
-                )}
-            </div>
-        </section>
-    )
+  useEffect(() => {
+    fetchGenreTitle({
+      ...params,
+      type,
+      page,
+      limit: 40,
+    })
+  }, [page])
+
+  const onChange = (page: number) => {
+    push({ query: { ...query, page } })
+  }
+
+  return (
+    <section className={clsx("catalog", className)}>
+      <div className="catalog__container">
+        <h1 className="catalog__title">{title}</h1>
+        {/* <div className="catalog__filters">
+          <Button>Фильтры</Button>
+          <span>сортировать по: (select)</span>
+        </div> */}
+        {isFetching && (
+          <div className="loader--container">
+            <Spinner />
+          </div>
+        )}
+        {isError && <p className="title--error">Упс.. Что-то пошло не так (⌣̩̩́_⌣̩̩̀)</p>}
+        {data && !isFetching && (
+          <div className="catalog__movies-list">
+            {data?.docs.map((item, i) => (
+              <MovieItem className="catalog__movies-item" key={i} item={item} />
+            ))}
+          </div>
+        )}
+        {data && <Pagination onChange={onChange} page={page} total={data?.pages} />}
+      </div>
+    </section>
+  )
 }
 
 export default Catalog
