fa493f79157203c9c7689c569bdedd4b096a1eee
Sysoev Bogdan <112561940+Lundoger@users.noreply.github.com>
2024-05-22 21:57:14 +0300

    Update Header.tsx

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -24,7 +24,7 @@ const Header = () => {
                 <Menu />
                 <div className="header__actions">
                     <SearchButton />
-                    <Account />
+                    //<Account />
                     <BurgerButton />
                 </div>
             </div>
